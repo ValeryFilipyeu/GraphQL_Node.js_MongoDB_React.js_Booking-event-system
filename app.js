4de9ef807101424d3e71a7f3c704d40956119584
Valery Filipyev <valeranuzhdin1998@mail.ru>
2019-10-01 17:39:57 +0300

    Schemas & Resolvers

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-  res.send('Hello world!');
-});
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Cooking", "Sailing", "All-Night Coding"];
+      },
+      createEvent: args => {
+        return args.name;
+      }
+    },
+    graphiql: true
+  })
+);
 
 app.listen(3000);
