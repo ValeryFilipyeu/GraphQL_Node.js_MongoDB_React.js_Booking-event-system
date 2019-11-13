e4fff37bb6b50218b962a4928e5cdbf457fb5159
Valery Filipev <valeranuzdin1998@mail.ru>
2019-11-13 22:34:03 +0300

    reformat a little bit

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,14 +1,14 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const User = require('../../models/user');
+const User = require("../../models/user");
 
 module.exports = {
   createUser: async args => {
     try {
       const existingUser = await User.findOne({ email: args.userInput.email });
       if (existingUser) {
-        throw new Error('User exists already.');
+        throw new Error("User exists already.");
       }
       const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
 
@@ -24,18 +24,22 @@ module.exports = {
       throw err;
     }
   },
-  login: async ({email, password}) => {
+  login: async ({ email, password }) => {
     const user = await User.findOne({ email });
     if (!user) {
-      throw new Error('User does not exist!');
+      throw new Error("User does not exist!");
     }
     const isEqual = await bcrypt.compare(password, user.password);
     if (!isEqual) {
-      throw new Error('Password is incorrect!');
+      throw new Error("Password is incorrect!");
     }
-    const token = jwt.sign({userId: user.id, email: user.email}, 'somesupersecretkey', {
-      expiresIn: '1h'
-    });
-    return { userId: user.id, token, tokenExpiration: 1 }
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      "somesupersecretkey",
+      {
+        expiresIn: "1h"
+      }
+    );
+    return { userId: user.id, token, tokenExpiration: 1 };
   }
 };
--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,20 +1,20 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const authHeader = req.get('Authorization');
+  const authHeader = req.get("Authorization");
   if (!authHeader) {
     req.isAuth = false;
     return next();
   }
-  const token = authHeader.split(' ')[1];
-  if (!token || token === '') {
+  const token = authHeader.split(" ")[1];
+  if (!token || token === "") {
     req.isAuth = false;
     return next();
   }
 
   let decodedToken;
   try {
-    decodedToken = jwt.verify(token, 'somesupersecretkey');
+    decodedToken = jwt.verify(token, "somesupersecretkey");
   } catch (err) {
     req.isAuth = false;
     return next();
